339a53fef55cdb73b4051a226c2292c2204ceb1a
Tran Thinh <tbnorder@gmail.com>
2024-05-23 16:16:04 +0700

    Create Project

--- src/main/java/com/teslusko/quizapp/QuestionController.java
+++ src/main/java/com/teslusko/quizapp/QuestionController.java
@@ -1,11 +1,16 @@
 package com.teslusko.quizapp;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("question")
 public class QuestionController {
 
+    @RequestMapping("allQuestions")
+    public String getAllQuestions() {
+        return "All questions";
+    }
 
 
 }
